678c67f916524ea0467e0863819d1f98629a031e
Brandon Alan Rodríguez <br4nd0n4l4n@gmail.com>
2016-11-12 11:09:54 -0600

    https

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app             = express();
 
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '192.168.56.101');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   // Request headers you wish to allow
