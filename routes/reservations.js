64056dd780225b7918c65d7261c0efd3983d8636
Brandon Alan Rodríguez <br4nd0n4l4n@gmail.com>
2016-11-12 12:41:07 -0600

    htpps

--- routes/reservations.js
+++ routes/reservations.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
 
   var server = require('https').Server(app);
   var io = require('socket.io')(server);
-  server.listen(443);
+  server.listen(4433);
 
   io.on('connection', function (socket) {
     console.log(socket);
